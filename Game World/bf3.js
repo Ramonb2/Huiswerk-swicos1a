a95d45ebe01fbf61cba6a9d76efbf6dc40e2dbf7
Ramonb2 <ramonb2@hotmail.nl>
2014-12-18 09:32:07 +0100

    ok

---
+++ Game World/bf3.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+  $("#1 > img").click(function(){
+    $("#1 > img").animate({
+      height:'640px',
+      width:'560px'
+    });
+  });
+});
+$(document).ready(function(){
+  $("#1 > img").dblclick(function(){
+    $("#1 > img").animate({
+      height:'320px',
+      width:'280px'
+    });
+  });
+});
+$(document).ready(function(){
+  $("#2 > img").click(function(){
+    $("#2 > img").animate({
+      height:'640px',
+      width:'560px'
+    });
+  });
+});
+$(document).ready(function(){
+  $("#2 > img").dblclick(function(){
+    $("#2 > img").animate({
+      height:'320px',
+      width:'280px'
+    });
+  });
+});
+$(document).ready(function(){
+  $("#3 > img").click(function(){
+    $("#3 > img").animate({
+      height:'640px',
+      width:'560px'
+    });
+  });
+});
+$(document).ready(function(){
+  $("#3 > img").dblclick(function(){
+    $("#3 > img").animate({
+      height:'320px',
+      width:'280px'
+    });
+  });
+});
--- java site 1 - kopie/WoT.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-  $("#main > img").click(function(){
-    $("#main > img").fadeOut(500);
-  });
-});
